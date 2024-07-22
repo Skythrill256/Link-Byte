2f60099232866c39677486993dbe8785110ecbe5
Skythrill256 <anjishnuganguly773@gmail.com>
2024-07-22 23:43:45 +0530

    Fourth commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className='supports-backdrop-blur:bg-background/60 z-20 w-full shadow bg-background/95 backdrop-blur'>
       <div className='mx-8 flex h-14 items-center'>
         <small className='text-muted-foreground'>
-        
+          <h1 className="text-lg">Made by Anjishnu Ganguly</h1>
         </small>
       </div>
     </div>
